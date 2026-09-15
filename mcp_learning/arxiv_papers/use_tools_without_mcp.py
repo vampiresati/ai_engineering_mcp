0d01002420c5c91d5b0de6c5fbed13f2f638907c
Satvir <sssatvir12@gmail.com>
2026-09-15 16:56:02 +0530

    Refactor `use_tools_without_mcp.py` to add `execute_tool` function for tool execution and enhance response handling.

--- mcp_learning/arxiv_papers/use_tools_without_mcp.py
+++ mcp_learning/arxiv_papers/use_tools_without_mcp.py
@@ -4,21 +4,26 @@ mapping_tool_function = {
     "search_papers": search_papers,
     "extract_info": extract_info
 }
+def execute_tool(tool_name,arguments):
+    function = mapping_tool_function[tool_name]
+    result = function(**arguments)
+    if result is None:
+        result = "The operation completed but didn't return any results."
+    elif isinstance(result, list):
+        result = ', '.join(result)
+        result = json.dumps(result, indent=2)
+    else:
+        result = str(result)
+    return result
 
 from ollama import chat
-response = chat(
-    model="qwen3:4b",
-    messages=[
-        {
+
+response = chat(model="qwen3:4b",
+            messages=[{
             "role": "user",
-            "content": "Find for paper id 2609.15922v1 directories"
-        }
-    ],
-    tools=tools
-)
+            "content": "Find for paper id 2609.15922v1 directories"}],tools=tools)
 # print('response')
 # print(response)
-
 
 if response.message.tool_calls:
     for tool_call in response.message.tool_calls:
@@ -26,7 +31,5 @@ if response.message.tool_calls:
         arguments = tool_call.function.arguments
         print("Tool:", tool_name)
         print("Arguments:", arguments)
-        function = mapping_tool_function[tool_name]
-        result = function(**arguments)
-#
+        result=execute_tool(tool_name,arguments)
         print("Result:", result)

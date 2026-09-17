ed088ed35c8396b148b3928564f8451104afbc17
Satvir <sssatvir12@gmail.com>
2026-09-17 16:11:04 +0530

    Update `coding_prompts.py` docstrings for Angular and release review functions

--- mcp_learning/coding_prompts/coding_prompts.py
+++ mcp_learning/coding_prompts/coding_prompts.py
@@ -12,13 +12,13 @@ COMPANY = os.getenv("COMPANY")
 
 @mcp.prompt()
 def review_angular_code(ticket_number: str) -> str:
-    """Review a piece of code."""
+    """Generate a prompt to review angular changes related to a ticket"""
     return f"commits of {ticket_number} into angular what is done in last merge of this , i need screen to screen display with url {URL} with project {PROJECT} and company id {COMPANY},just of screen to screen display changes.do not open browser and do not run tests\n\n"
 
 
 @mcp.prompt()
 def review_release_code(ticket_number: str) -> str:
-    """Review a piece of code."""
+    """Generate a prompt to review release/release changes related to a ticket"""
     return f"commits of {ticket_number} into release/release what is done in last merge of this , i need screen to screen display with url {URL} with project {PROJECT} and company id {COMPANY},just of screen to screen display changes.do not open browser and do not run tests\n\n"
 
 

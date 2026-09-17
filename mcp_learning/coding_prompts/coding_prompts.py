dcc6d8bbfdd8e13f2a0eacf385f3058a164d50c8
Satvir <sssatvir12@gmail.com>
2026-09-17 16:09:51 +0530

    Add `coding_prompts` module for Angular and release code review prompts
    Introduce `coding_prompts.py` script with `MCPServer` integration to support ticket-specific Angular and release code review prompts. Includes `.env` configuration for dynamic URL, project, and company ID usage.

---
+++ mcp_learning/coding_prompts/coding_prompts.py
@@ -0,0 +1,27 @@
+from mcp.server import MCPServer
+mcp = MCPServer("Codingprompts")
+
+import os
+from dotenv import load_dotenv
+load_dotenv("/home/satvir/ai_engineering/mcp_learning/coding_prompts/.prompts_env")
+
+URL = os.getenv("URL")
+PROJECT = os.getenv("PROJECT")
+COMPANY = os.getenv("COMPANY")
+
+
+@mcp.prompt()
+def review_angular_code(ticket_number: str) -> str:
+    """Review a piece of code."""
+    return f"commits of {ticket_number} into angular what is done in last merge of this , i need screen to screen display with url {URL} with project {PROJECT} and company id {COMPANY},just of screen to screen display changes.do not open browser and do not run tests\n\n"
+
+
+@mcp.prompt()
+def review_release_code(ticket_number: str) -> str:
+    """Review a piece of code."""
+    return f"commits of {ticket_number} into release/release what is done in last merge of this , i need screen to screen display with url {URL} with project {PROJECT} and company id {COMPANY},just of screen to screen display changes.do not open browser and do not run tests\n\n"
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
+#     print(get_weather("delhi"))
